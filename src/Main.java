c91ce68b58f5072b1539ba597db150b30f7d16ae
Olga Stepanova <os20142020@gmail.com>
2021-08-13 04:01:09 +0200

    Diners Club - Carte Blanche

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4539373183345018879";
+        String number = "30267763193942";
         System.out.println(String.format("Result is %s%n", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
